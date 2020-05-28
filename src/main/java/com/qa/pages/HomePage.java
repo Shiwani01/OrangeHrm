74dcbe8f5f1aa4780b09b1f6b2e37cfe5b32f1fd
Shiwani01 <stummulwar@gmail.com>
2020-05-28 10:05:54 +0530

    Change in Homepage

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,22 @@
 package com.qa.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+	
+	@FindBy(id="menu_dashboard_index")
+	WebElement dashboard;
+	
+	@FindBy(xpath="//span[contain(text(),'Assign Leave')]")
+	
+	WebElement assignleave;
+	
+	@FindBy(xpath="//span[contain(text(),'Leave List')]")
+	WebElement leavelist;
+	
+	
 
 }
